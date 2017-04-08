f62ee614f94714c38408491a124544139453211a
kfu95 <kevin.fu.95@gmail.com>
2017-04-08 14:38:51 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.use(express.static(__dirname+'/styles.css'));
+app.get('/Bitcamp.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "Bitcamp.html" );
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      venue:req.query.venues,
+      
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+
+})
