fcdc203f102369d3d2bd0ffb086337e7c76ea045
Ayman Karim <akarim1@terpmail.umd.edu>
2017-04-08 06:15:50 +0000

    Added more descriptive comments

--- events_from_artist.py
+++ events_from_artist.py
@@ -2,30 +2,33 @@ import json,requests
 
 #getting the artist's songkick ID
 def get_artist_id(artistName):
+    #base URL
     url = 'http://api.songkick.com/api/3.0/search/artists.json?'
+    
     #query parameters
     params = dict(
             query = artistName,
     	    apikey='s9TgD3sUNEyDRjbc'
     )
-    #calling API
+    
+    #calling the SongKick API
     resp = requests.get(url=url, params=params)
     data = json.loads(resp.text)
+    
     #getting the artist's id from the returned JSON object
     artist_id = data['resultsPage']['results']['artist'][0]['id']
     return artist_id
-    
 
 
-
+#getting an artist's event calendar 
 def get_events_for_artist(artistid):
-    #Getting the artist's event calendar
+    #base URL
     url = 'http://api.songkick.com/api/3.0/artists/' + str(artistid) + '/calendar.json?apikey=s9TgD3sUNEyDRjbc'
-
+    #Calling SongKick API
     resp = requests.get(url=url)
     data = json.loads(resp.text)
     
-    #list to store events (dicts/hashes)
+    #list to store events 
     events_arr = []
     
     for event in data['resultsPage']['results']['event']:
