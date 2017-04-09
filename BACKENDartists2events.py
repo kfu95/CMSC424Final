2d5d05264666693c07f14a5e66d5565da9004025
Ayman Karim <akarim1@terpmail.umd.edu>
2017-04-09 05:04:00 +0000

    added file for connecting backend and frontend

--- BACKENDartists2events.py
+++ BACKENDartists2events.py
@@ -2,15 +2,16 @@ import events_from_artist as efa
 import json
 import sys
 
-#displayname of event
-#id of event
-
+#getting the correct arguments from command line
 artist_name = sys.argv[1]
 start_date = sys.argv[2]
 end_date = sys.argv[3]
 
+#getting songkick ID for artist
 artist_id = efa.get_artist_id(artist_name)
 
+#getting a list of dictionaries
+# where each dict is a concert
 concert_dict = efa.get_events_for_artist(artist_id,start_date,end_date)
 
 print(json.dumps(concert_dict))
