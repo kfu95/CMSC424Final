4fdc0ec5e6334b648c2ec2135edc68011883dcd6
Ayman Karim <akarim1@terpmail.umd.edu>
2017-04-09 05:19:16 +0000

    Default

--- server.js
+++ server.js
@@ -12,73 +12,70 @@ app.get('/Bitcamp.html', function (req, res) {
 
 app.set('view engine', 'ejs');
 
-app.get('/events', function (req, res) {
-   // Prepare output in JSON format
-   var venue = req.query.venue;
-   var startdate = req.query.startdate;
-   var enddate = req.query.enddate;
-   
-   // RUN OUR PYTHON SCRIPT
-   var spawn = require("child_process").spawn;
-   // SCRIPT TO TURN A VENUE ID INTO EVENTS GIVEN START DATE AND END DATE
-   var process = spawn('python',["BACKENDvenue2events.py", venue, startdate, enddate]);
-   
-   // OBJECT TO STORE DATA
-   str = {data:""}
-   
-   // BUFFER MAY BE TOO SMALL, SO THIS INNER FUNCTION MAY BE CALLED MULTIPLE TIMES
-    process.stdout.on('data', function (data){
-        // WHEN DATA IS RECEIVED IN THE BUFFER, ADD IT TO THE STR OBJECT
-        console.log(str.data.length)
-        str.data += data.toString()
-    });
-   
-   // WHEN OUR PYTHON SCRIPT FINISHES
-    process.on('close', function(code) {
-   
-        // GET OUR JSON RESPONSE READY
-       var response = {
-          venue:venue,
-          data:JSON.parse(str.data)
-       };
-       
-       // RETURN OUR JSON RESPONSE
-      res.end(JSON.stringify(response));
-   
-    });
-    
-   
-   
-})
 
 app.get('/events', function (req, res) {
    
-   // GET THE EVENT ID(S) TO GET ARTISTS FOR
-   var artist = req.query.artist;
-    var startdate = req.query.startdate;
-   var enddate = req.query.enddate;
-   
-   console.log(req.query);
-   
-   console.log(artist);
-   
-   // PYTHON SCRIPT TO GET ARTISTS FROM EVENT IDS
-   var spawn = require("child_process").spawn;
-   var process = spawn('python',["BACKENDartists2events.py", artist,startdate,enddate]);
-   
-   str = {data:""}
-   
-   process.stdout.on('data', function (data){
-       // GET THE DATA
-        str.data += data.toString();
-   });
-   
-   process.on('close', function(code) {
-        // RENDER OUR EMBEDDED JAVASCRIPT WITH OUR JSON DATA
-        response = JSON.parse(str.data);
-        res.end(JSON.stringify(response));
-    });
-        
+   if (typeof req.query.artist != 'undefined') {
+       // GET THE EVENT ID(S) TO GET ARTISTS FOR
+       var artist = req.query.artist;
+       var startdate = req.query.startdate;
+       var enddate = req.query.enddate;
+       
+       console.log(req.query);
+       
+       console.log(artist);
+       
+       
+       // PYTHON SCRIPT TO GET ARTISTS FROM EVENT IDS
+       var spawn = require("child_process").spawn;
+       var process = spawn('python',["BACKENDartists2events.py", artist,startdate,enddate]);
+       
+       str = {data:""}
+       
+       process.stdout.on('data', function (data){
+           // GET THE DATA
+            str.data += data.toString();
+       });
+       
+       process.on('close', function(code) {
+            // RENDER OUR EMBEDDED JAVASCRIPT WITH OUR JSON DATA
+            response = JSON.parse(str.data);
+            res.end(JSON.stringify(response));
+        });
+   } else {
+        var venue = req.query.venue;
+       var startdate = req.query.startdate;
+       var enddate = req.query.enddate;
+       
+       // RUN OUR PYTHON SCRIPT
+       var spawn = require("child_process").spawn;
+       // SCRIPT TO TURN A VENUE ID INTO EVENTS GIVEN START DATE AND END DATE
+       var process = spawn('python',["BACKENDvenue2events.py", venue, startdate, enddate]);
+       
+       // OBJECT TO STORE DATA
+       str = {data:""}
+       
+       // BUFFER MAY BE TOO SMALL, SO THIS INNER FUNCTION MAY BE CALLED MULTIPLE TIMES
+        process.stdout.on('data', function (data){
+            // WHEN DATA IS RECEIVED IN THE BUFFER, ADD IT TO THE STR OBJECT
+            console.log(str.data.length)
+            str.data += data.toString()
+        });
+       
+       // WHEN OUR PYTHON SCRIPT FINISHES
+        process.on('close', function(code) {
+       
+            // GET OUR JSON RESPONSE READY
+           var response = {
+              venue:venue,
+              data:JSON.parse(str.data)
+           };
+           
+           // RETURN OUR JSON RESPONSE
+          res.end(JSON.stringify(response));
+       
+        });
+   }
    
 })
 
